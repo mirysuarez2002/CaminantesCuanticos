bfd62c1d49eea6e2f08d0b14f130017020ff7ce8
Miryam Suárez <mirysuarez2002@gmail.com>
2026-06-24 11:55:37 +0200

    Cambios

--- 2D/AerSimulator_2-dim.py
+++ 2D/AerSimulator_2-dim.py
@@ -4,23 +4,24 @@ import csv
 import os
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import Statevector
+from qiskit_aer import AerSimulator
 
 #Se definen los cúbits que se van a necesitar para la posición, tanto en una dimensión como en otra, ya que darán los mismo pasos en las dos direcciones. 
 #Estos se corresponden exactamente con el logaritmo en base dos de doble del número de pasos más uno, que se corresponde con la posición 0.
-q_posicion_1D = 3
+q_posicion_1D = 5
 #Se calcula el número máximo de posiciones que se pueden escribir con el número de cúbits definido
 num_max_pos = 2**q_posicion_1D
-pasos = 2
+pasos = 10
 #Se calcula el centro. Sin embargo, no será el centro exacto, debido a que siempre habrá posiciones potencia de 2,
 #por lo que la posición centro no tendrá el mismo número de posiciones a cada lado.
 centro = num_max_pos // 2 
 #Se define el archivo que guardará los valores de las posiciones y sus probabilidades.
-archivo = "hadamard_teorico_2.csv"
+archivo = "hadamard_simOrdenador_10.csv"
 directorio = os.path.dirname(__file__)
 ruta = os.path.join(directorio,archivo)
 
 #Se define el circuito cuántico con los cúbits de posición de ambas dimensiones más dos cúbits moneda, uno por dimensión
-qc = QuantumCircuit(q_posicion_1D*2 + 2) 
+qc = QuantumCircuit(q_posicion_1D*2 + 2) #Añadimos los qubits de la moneda
 
 #Se necesita conocer el valor en binario del centro, para hacer más eficiente el programa
 centro_binario = format(centro, f'0{q_posicion_1D}b') #Se pasa el número a binario y se añaden los ceros que sean necesarios para completar la cadena de qubits
@@ -89,20 +90,30 @@ for _ in range(pasos):
     for i in range(2, q_posicion_1D*2+2):
         qc.x(i)
 
-#Obtenemos las probabilidades con Statevector
-estado = Statevector.from_instruction(qc) #Calcula la función de onda completa, sin medidas
-probs = estado.probabilities_dict() #Escribe un listado con los bits y las probabilidades de cada uno
+
+#Añadimos los operadores de medida
+qc.measure_all()
+
+#Llamamos al simulador
+sim=AerSimulator()
+#Definimos el resultado con el simulador e indicando el número de veces que queremos que se ejecute (shots)
+result=sim.run(qc, shots=10000).result()
+
+#Obtenemos cada resultado
+counts=result.get_counts()
+
+#Sumamos cuantos valores hemos obtenido: Número total de shots
+total_shots = sum(counts.values()) #Número total de shots
 
 #Creamos el vector de probabilidades vacio
 pos_prob = np.zeros((num_max_pos, num_max_pos))
 
-for bit_pos, prob_pos in probs.items(): #Recorremos el lista, mirando cada bit y su probabilidad
+for bit_pos, n_veces in counts.items(): #Recorremos el lista, mirando cada bit y su probabilidad
     bit_sin_Control = bit_pos[:-2] #Se quitan los últimos dos cúbits, que son los cúbits moneda
     pos_decimal_horizontal = int(bit_sin_Control[q_posicion_1D:], 2) #Me quedo con la segunda mitad del string de cúbits de posiciones y pasamos a binario, que corresponde con la posición horizontal
     pos_decimal_vertical = int(bit_sin_Control[:q_posicion_1D], 2) #Hago lo mismo con la primera mitad. Es la posición vertical.
-    pos_prob [pos_decimal_horizontal, pos_decimal_vertical] += prob_pos #Sumamos todas las probabilidades de la posición, ignorando el estado moneda
-
-
+    prob = n_veces/total_shots #Calculamos la probabilidad dividiendo las veces que ha terminado en esa posición entre el número de shots
+    pos_prob [pos_decimal_horizontal, pos_decimal_vertical] += prob #Sumamos todas las probabilidades de la posición, ignorando el estado moneda
 
 #Abrimos el archivo antes definido y copiamos los datos
 with open(ruta, mode = 'w', newline = '', encoding = 'utf-8') as file:
@@ -110,6 +121,8 @@ with open(ruta, mode = 'w', newline = '', encoding = 'utf-8') as file:
     writer.writerow(['Posición Horizontal', 'Posición Vertical', 'Probabilidad'])
     for (pos_decimal_horizontal, pos_decimal_vertical), prob in np.ndenumerate(pos_prob):
         writer.writerow([pos_decimal_horizontal - centro, pos_decimal_vertical - centro, prob])
+
+
 
 #Compruebo que las probabilidades suman 1
 #suma = np.sum(pos_prob)
@@ -126,8 +139,6 @@ Z_pares = Z_completa[quitarImpares, :][:, quitarImpares]
 
 #Elimino los valores de probabilidad para posiciones mayores, en valor absoluto, al número de pasos, para que se vea mejor la gráfica 3D
 indices_dentro = np.where((posiciones_pares >= -pasos) & (posiciones_pares <= pasos))[0]
-
-#Se definen los valores de la gráfica 3D
 
 Z = Z_pares[np.ix_(indices_dentro, indices_dentro)]
 pos_final = posiciones_pares[indices_dentro]
@@ -148,10 +159,7 @@ ax.set_zlabel('Probabilidad')
 #Para 2 pasos 0.25
 #Para 6 pasos 0.08
 #Para 10 pasos 0.07
-max_z = 0.25
-
-#La primera vez que lo ejecuto si debo buscar el máximo, para saber cuál es:
-#max_z = np.max(z)
+max_z = 0.07
 
 #Pongo el límite de cada eje
 ax.set_xlim(-(pasos), pasos)
@@ -180,9 +188,11 @@ im = ax.imshow(Z, cmap='viridis', origin='lower',
 ax.set_xlabel('Posición en el Eje X')
 ax.set_ylabel('Posición en el Eje Y')
 
+
 marcas = np.arange(pos_final[0], pos_final[-1] + 1, 4)
 ax.set_xticks(marcas)
 ax.set_yticks(marcas)
+
 
 ax.set_aspect('equal')
 
