4e35be800f3faae8924967d79579804c1724f0e8
Miryam Suárez <mirysuarez2002@gmail.com>
2026-06-24 12:30:45 +0200

    Actualizar

--- 2D/CalculoCúbits_Prof_Puertas_2D/PropiedadesCircuito_2-dim.py
+++ 2D/CalculoCúbits_Prof_Puertas_2D/PropiedadesCircuito_2-dim.py
@@ -30,57 +30,80 @@ lista_qubits_transpilado = []
 
 #Se define el circuito en función del número de pasos
 def circuito(pasos):
-    q_posicion = math.ceil(math.log2(2*pasos+1))
-    num_max_pos = 2**q_posicion
+    q_posicion_1D = math.ceil(math.log2(2*pasos+1))
+    num_max_pos = 2**q_posicion_1D
     centro = num_max_pos // 2 
 
     #Se define el circuito cuántico con los cúbits de posición más un cúbit moneda
-    qc = QuantumCircuit(q_posicion + 1) #Añadimos el qubit del spin
+    qc = QuantumCircuit(q_posicion_1D + 1) 
 
     #Se necesita conocer el valor en binario del centro, para hacer más eficiente el programa
-    centro_binario = format(centro, f'0{q_posicion}b') #Se pasa el número a binario y se añaden los ceros que sean necesarios para completar la cadena de qubits
+    centro_binario = format(centro, f'0{q_posicion_1D}b') #Se pasa el número a binario y se añaden los ceros que sean necesarios para completar la cadena de qubits
     #Se coloca al caminante en el centro del sistema, variando los qubits necesarios de 0 a 1
     #Se invierte la cadena de bit del centro, ya que qiskit los lee invertidos 
+    #Se pone en la posición centro en ambas direcciones
     for i, bit in enumerate(reversed(centro_binario)):
         if bit == '1':
-            qc.x(i + 1) #Se suma uno, debido al qubit de control
+            qc.x(i + 2) #Se suma dos, debido a los qubits de control
+            qc.x(i + 2 + q_posicion_1D) #Se suma los dos qubits de control y los 6 qubits de la dirección horizontal, para inicializar también la vertical en 0
     #Este proceso se podría usar para cualquier posición inicial, pero en los casos que se usan en este caso,
-    #donde se busca siempre que el caminante empiece en el centro, se podría añadir una sola puerta NOT al cúbit de más peso.
+    #donde se busca siempre que el caminante empiece en el centro, se podría añadir una sola puerta NOT al cúbit de más peso para ambas dimension.
 
 
-    #Inicializamos el estado inicial en 1/raiz(2)(|arriba>+i|abajo>):
-    #Conseguimos el estado 1/raiz(2)(|arriba>+|abajo>)
+    #Inicializamos el estado inicial:
+    #Conseguimos el estado 1/raiz(2)(|izq>+|der>)
     qc.h(0)
-    #Tras esto, rotamos el estado con la matriz P, con theta=pi/2
+
+    #Conseguimos el estado 1/raiz(2)(|abajo>+|arriba>)
+    qc.h(1)
+
+    #Tras esto, rotamos el estado con la matriz P, con theta=pi/2 en ambos qubits de control
     qc.p(np.pi/2, 0)
+    qc.p(np.pi/2, 1)
+
 
 
 
     for _ in range(pasos):
         #Aplicamos la moneda
         qc.h(0) 
+        qc.h(1)
 
-        #Movemos a la derecha cuando sea necesario: Usamos una puerta multicontrolada para cada qubit
-        for i in range(q_posicion, 0, -1):
-            qc.mcx(list(range(i)), i)
+        #Movemos a la derecha cuando sea necesario: Usamos una puerta multicontrolada para cada qubit 
+        for i in range(q_posicion_1D+1, 1, -1):
+            controles = [0] + list(range(2, i))
+            qc.mcx(controles, i)
+
+        #Movemos hacia arriba cuando sea necesario: Usamos una puerta multicontrolada para cada qubit 
+        for i in range(q_posicion_1D*2+1, q_posicion_1D+1, -1):
+            controles = [1] + list(range(q_posicion_1D+2, i))
+            qc.mcx(controles, i)
+
+
         
-        #Movemos a la izquierda cuando sea necesario: Usamos una puerta multicontrolada para cada qubit
+        #Movemos a la izquierda y hacia abajo cuando sea necesario: Usamos una puerta multicontrolada para cada qubit
         #Aplicamos las puertas X
         qc.x(0)
-        for i in range(1, q_posicion):
+        qc.x(1)
+        for i in range(2, q_posicion_1D*2+2):
             qc.x(i)
 
 
-        #Movemos al caminante con puertas multicontroladas
-        for i in range(q_posicion, 0, -1):
-            qc.mcx(list(range(i)), i)
+        for i in range(q_posicion_1D+1, 1, -1):
+            controles = [0] + list(range(2, i))
+            qc.mcx(controles, i)
+
+        for i in range(q_posicion_1D*2+1, q_posicion_1D+1, -1):
+            controles = [1] + list(range(q_posicion_1D+2, i))
+            qc.mcx(controles, i)
         
 
-        #Volvemos a aplicar puertas X para volver a los valores reales
-        for i in range(1, q_posicion):
+        #Aplicamos las puertas X
+        qc.x(0)
+        qc.x(1)
+        for i in range(2, q_posicion_1D*2+2):
             qc.x(i)
-        qc.x(0) 
-    #Devolvemos el circuito correspondiente
+
     return qc
 
 
@@ -92,7 +115,7 @@ with open(ruta, mode = 'w', newline = '', encoding = 'utf-8') as f_estandar, ope
 
     writer_estandar.writerow(['Pasos', 'Qubits', 'Profundidad', 'Puertas Totales'])
     writer_transpilado.writerow(['Pasos', 'Qubits', 'Profundidad', 'Puertas Totales'])
-
+    
     #Para cada valor de paso llamamos a la función circuito y calculamos sus propiedades usando las funciones de qiskit
     for p in lista_pasos:
         circ = circuito(p)
@@ -116,22 +139,26 @@ with open(ruta, mode = 'w', newline = '', encoding = 'utf-8') as f_estandar, ope
         lista_puertas_transpilado.append(num_puertas_t)
         lista_qubits_transpilado.append(qubits_t)
 
+
         #Se escriben todos los valores en los archivos
         writer_estandar.writerow([p, qubits, profundidad, num_puertas])
         writer_transpilado.writerow([p, qubits_t, profundidad_t, num_puertas_t])
 
+
 #Se representan las figuras correspondientes
 
-plt.figure(figsize=(8, 5))
+plt.figure(figsize=(7, 5))
 plt.plot(lista_pasos, lista_qubits, color="black", linestyle="-", label="Teórico")
 plt.plot(lista_pasos, lista_qubits_transpilado, color="#377eb8", linestyle="-", label="Transpilado (Kingston)")
-plt.tick_params(labelsize=13)
-plt.xlabel("Número de Pasos", fontsize=15)
-plt.ylabel("Número de Cúbits", fontsize=15)
+plt.tick_params(labelsize=16)
+plt.xlabel("Número de Pasos", fontsize=20)
+plt.ylabel("Número de Cúbits", fontsize=20)
 plt.xlim(0, 11)
 plt.grid(True, linestyle="--", alpha=0.5)
-plt.legend(fontsize=13)
+plt.legend(fontsize=16)
+plt.tight_layout()
 plt.show()
+
 
 plt.figure(figsize=(7, 5))
 plt.plot(lista_pasos, lista_profundidad, color="black", linestyle="-", label="Teórico")
